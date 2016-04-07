4db7f24dc9b175b9503af243f99febd040916d0c
Alexander Wallin <office@alexanderwallin.com>
2016-04-07 08:25:09 +0200

    Removes unnecessary exports from json2pot

--- src/json2pot.js
+++ src/json2pot.js
@@ -2,7 +2,7 @@
 import deepExtend from 'deep-extend';
 
 // Default gettext json options
-export const GETTEXT_JSON_OPTS = {
+const GETTEXT_JSON_OPTS = {
   charset: 'utf8',
   headers: {
     'pot-creation-date': new Date().toString(),
@@ -16,10 +16,10 @@ export const GETTEXT_JSON_OPTS = {
 
 const GENERATED_INFO = `#\n# Generated using json2pot\n#`;
 
-export const toTitleSnakeCase = str =>
+const toTitleSnakeCase = str =>
   str.replace(/([^\w]|^)([\w])/g, x => x.toUpperCase());
 
-export const headers = (meta = {}) =>
+const headers = (meta = {}) =>
   Object.keys(meta)
     .reduce((headerList, key) => headerList.concat({
       name: [toTitleSnakeCase(key)],
@@ -29,27 +29,27 @@ export const headers = (meta = {}) =>
     .filter(str => str.trim())
     .join('\n');
 
-export const comment = message => (
+const comment = message => (
   message.comment ? `# ${message.comment}` : ''
 );
 
-export const sources = message => (
+const sources = message => (
   message.sources
     ? message.sources.sort().map(loc => `#: ${loc}`).join('\n')
     : ''
 );
 
-export const context = message => (
+const context = message => (
   message.msgctxt ? `msgctxt "${message.msgctxt}"` : ''
 );
 
-export const strings = message => (
+const strings = message => (
   message.msgid_plural
     ? `msgid "${message.msgid}"\nmsgid_plural "${message.msgid_plural}"\nmsgstr[0] ""\nmsgstr[1] ""`
     : `msgid "${message.msgid}"\nmsgstr ""`
 );
 
-export const messageDef = message =>
+const messageDef = message =>
   [comment, sources, context, strings]
     .map(fn => fn(message))
     .filter(str => str.trim())
