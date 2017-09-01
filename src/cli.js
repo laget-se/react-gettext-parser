607c6d756259baecc361de23783ea4cf1618f2f3
Alexander Wallin <office@alexanderwallin.com>
2017-09-01 19:02:54 +0100

    #22: Adds trim options to CLI

--- src/cli.js
+++ src/cli.js
@@ -16,10 +16,28 @@ const args = require('yargs')
     alias: 'config',
     description: 'Path to a react-gettext-parser config file',
   })
+  .option('trim', {
+    type: 'boolean',
+    description: 'Trims extracted strings from surrounding whitespace',
+  })
+  .option('trim-lines', {
+    type: 'boolean',
+    description: 'Trims each line in extracted strings from surrounding whitespace',
+  })
+  .option('trim-newlines', {
+    type: 'boolean',
+    description: 'Trims extracted strings from new-lines',
+  })
   .argv;
 
 const filesGlob = args._;
-let opts = { output: args.output };
+
+let opts = {
+  output: args.output,
+  trim: args.trim,
+  trimLines: args['trim-lines'],
+  trimNewlines: args['trim-newlines'],
+};
 
 if (args.config) {
   const configs = require(path.join(process.cwd(), args.config));
